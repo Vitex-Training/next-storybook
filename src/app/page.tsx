55b15b9aa7ced589ab4924dcb8a0b531bd48d774
unknown <nishyhai@vitex.asia>
2024-12-15 14:48:44 +0700

    remove draft code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,10 @@
-import {
-  AppAccordion,
-  AppAccordionContent,
-  AppAccordionItem,
-  AppAccordionTrigger,
-} from 'src/shared/components/accordion/AppAccordion';
-
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        <AppAccordion className='w-full' collapsible={false} type='single'>
-          <AppAccordionItem value='item-1'>
-            <AppAccordionTrigger>Oh my god oh hell nah man</AppAccordionTrigger>
-            <AppAccordionContent>What the fuck man</AppAccordionContent>
-          </AppAccordionItem>
-          <AppAccordionItem value='item-2'>
-            <AppAccordionTrigger iconPosition='start'>vjppro</AppAccordionTrigger>
-            <AppAccordionContent>Waasjcnk</AppAccordionContent>
-          </AppAccordionItem>
-          <AppAccordionItem value='item-3'>
-            <AppAccordionTrigger>Ahiahi</AppAccordionTrigger>
-            <AppAccordionContent>Omy god</AppAccordionContent>
-          </AppAccordionItem>
-        </AppAccordion>
+        <a className='font-mono font-bold' href='/counter'>
+          To counter screen
+        </a>
       </div>
     </main>
   );
