e7f2dc5c3bf4f99dc7c995f9f7d54f74eba5daec
unknown <nishyhai@vitex.asia>
2024-12-18 17:07:33 +0700

    restore main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import {
-  AppBreadcrumb,
-  AppBreadcrumbEllipsis,
-  AppBreadcrumbItem,
-  AppBreadcrumbLink,
-  AppBreadcrumbList,
-  AppBreadcrumbPage,
-  AppBreadcrumbSeparator,
-} from 'src/shared/components/breadcrumb/AppBreadcrumb';
-
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
@@ -15,25 +5,6 @@ export default function Home() {
         <a className='font-mono font-bold' href='/counter'>
           To counter screen
         </a>
-        <div>
-          <AppBreadcrumb>
-            <AppBreadcrumbList>
-              <AppBreadcrumbItem>
-                <AppBreadcrumbLink href='#'>Home</AppBreadcrumbLink>
-              </AppBreadcrumbItem>
-              <AppBreadcrumbSeparator />
-              <AppBreadcrumbEllipsis />
-              <AppBreadcrumbSeparator />
-              <AppBreadcrumbItem>
-                <AppBreadcrumbLink href='#'>Component</AppBreadcrumbLink>
-              </AppBreadcrumbItem>
-              <AppBreadcrumbSeparator />
-              <AppBreadcrumbItem>
-                <AppBreadcrumbPage>Dogshitet</AppBreadcrumbPage>
-              </AppBreadcrumbItem>
-            </AppBreadcrumbList>
-          </AppBreadcrumb>
-        </div>
       </div>
     </main>
   );
